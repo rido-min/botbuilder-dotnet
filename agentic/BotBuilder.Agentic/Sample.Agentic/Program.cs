e37c68aa71686a91e508fb93756da87f58b8050a
Ricardo Minguez Pablos (RIDO) <rmpablos@microsoft.com>
2025-10-20 10:38:05 -0700

    start agentic

---
+++ agentic/BotBuilder.Agentic/Sample.Agentic/Program.cs
@@ -0,0 +1,25 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Integration.AspNet.Core;
+using Microsoft.Bot.Connector.Authentication;
+using Sample.Agentic;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSingleton<BotFrameworkAuthentication, ConfigurationBotFrameworkAuthentication>();
+
+builder.Services.AddSingleton<IBotFrameworkHttpAdapter>(provider =>
+    new CloudAdapter(
+        provider.GetRequiredService<BotFrameworkAuthentication>(),
+        provider.GetRequiredService<ILogger<CloudAdapter>>()));
+
+builder.Services.AddTransient<IBot, EchoBot>();
+
+var app = builder.Build();
+
+app.MapPost("/api/messages", async (IBotFrameworkHttpAdapter adapter, IBot bot, HttpRequest request, HttpResponse response) =>
+    await adapter.ProcessAsync(request, response, bot));
+
+app.Run();
